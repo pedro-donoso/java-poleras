5746629943e88c8dd95dd29e0109fe1c9d66342d
pedro-donoso <pedro.donoso.r@usach.cl>
2024-03-20 00:13:41 -0300

    agrego metodo mostrarPolera

--- src/polera/Polera.java
+++ src/polera/Polera.java
@@ -7,16 +7,25 @@ public class Polera {
   String talla, marca;
   int precio;
 
-  ArrayList<Polera> poleras = new ArrayList<Polera>();
+  ArrayList<Polera> todasPoleras = new ArrayList<Polera>();
 
   // CONSTRUCTOR POR DEFECTO
   public Polera() {
   }
 
+  // CONSTRUCTOR QUE LLENA LOS DATOS DE LA CLASE
   public Polera(String talla, String marca, int precio) {
     this.talla = talla;
     this.marca = marca;
     this.precio = precio;
+  }
+
+  // AGREGO MÉTODO
+  public void mostrarPolera() {
+    for (Polera poleraObjeto : todasPoleras) {
+      System.out.println(
+          "Talla: " + poleraObjeto.talla + " Marca: " + poleraObjeto.marca + " Precio: " + poleraObjeto.precio);
+    }
   }
 
 }
